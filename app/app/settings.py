67bf980e4b14311f29393b3810c27267efecb803
Edmerson Pizarra <edmerpizarra@gmail.com>
2022-02-13 16:01:56 +0800

    feat: production ready config

--- app/app/settings.py
+++ app/app/settings.py
@@ -22,7 +22,9 @@ load_dotenv(dotenv_path=BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hvg9$qyu9cg^^^w2oq0v!xb_)sd5n!+3a-7b$qa3+5l42em2%1'
+# SECRET_KEY = 'django-insecure-hvg9$qyu9cg^^^w2oq0v!xb_)sd5n!+3a-7b$qa3+5l42em2%1'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -162,3 +164,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import django_heroku
+django_heroku.settings(locals())
